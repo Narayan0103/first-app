d6279aa216262e637df0ee5f3f3cfe88ea7f6166
EMIRATES\S416991 <narayan.kumar@emirates.com>
2020-05-10 15:07:20 +0400

    Initial Commit

--- src/main/java/com/first/app/controller/AppController.java
+++ src/main/java/com/first/app/controller/AppController.java
@@ -2,11 +2,11 @@ package com.first.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@Controller
+@RestController
 @RequestMapping("/apps")
 public class AppController {
 	private final AtomicInteger counter = new AtomicInteger(0);
